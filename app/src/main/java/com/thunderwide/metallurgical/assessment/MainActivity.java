dfa2c11c3ad844169bb4e135572513be75361efc
metallurgical <norlihazmey89@gmail.com>
2016-05-30 22:18:54 +0800

    init

---
+++ app/src/main/java/com/thunderwide/metallurgical/assessment/MainActivity.java
@@ -0,0 +1,18 @@
+package com.thunderwide.metallurgical.assessment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+/**
+ * Created by metallurgical on 5/30/16.
+ */
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate( Bundle savedInstaceState ) {
+        super.onCreate( savedInstaceState );
+        setContentView( R.layout.activity_main );
+    }
+}
